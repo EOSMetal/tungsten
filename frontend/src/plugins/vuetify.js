f307c31f429a111b885a601903bea24709eec757
Andres Berrios <andres.berrios.j@gmail.com>
2018-09-18 18:22:23 +0200

    Add Vuetify

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#FBC02D',
+    secondary: '#F9A825',
+    accent: '#448AFF',
+    error: '#EF5350',
+    warning: '#FFF176',
+    info: '#81D4FA',
+    success: '#4caf50'
+  }
+})
