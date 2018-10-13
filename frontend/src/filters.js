a41a312fe6013921bbe7635bdc38e461ca1025c6
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-13 22:02:03 +0200

    Fix display of dates

--- frontend/src/filters.js
+++ frontend/src/filters.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
 import moment from "moment";
 
-Vue.filter("date", ts => moment(parseInt(ts)).format());
-Vue.filter("dateFromNow", ts => moment(parseInt(ts)).fromNow());
+Vue.filter("date", ts => moment(parseInt(ts) * 1000).format());
+Vue.filter("dateFromNow", ts => moment(parseInt(ts) * 1000).fromNow());
