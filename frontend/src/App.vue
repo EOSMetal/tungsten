9a011abcd87fd7be28b804cf437cf3f8e14a523d
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-13 19:17:10 +0200

    Remove webflow.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,7 +7,5 @@ import "@/assets/css/normalize.css";
 import "@/assets/css/webflow.css";
 import "@/assets/css/tungsten.webflow.css";
 
-import "@/assets/js/webflow.js";
-
 export default {};
 </script>
