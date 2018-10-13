4669657ac2d45dd1665b18e62556297b42cc0709
Andres Berrios <andres.berrios.j@gmail.com>
2018-10-13 19:18:04 +0200

    Implement sidebar functionality for smaller screens

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,147 @@
+<template>
+  <nav role="navigation" class="tungsten-nav-menu">
+      <router-link :to="{name: 'home'}" class="tungsten-nav-link">
+        <div class="w-row">
+          <div class="w-col w-col-2">
+            <fa-icon icon="home" size="2x" class="nav-icon"/>
+          </div>
+          <div class="column-nav w-col w-col-10">
+            Home
+          </div>
+        </div>
+      </router-link>
+      <router-link :to="{name: 'bonds'}" class="tungsten-nav-link">
+        <div class="w-row">
+          <div class="w-col w-col-2">
+            <fa-icon icon="link" size="2x" class="nav-icon"/>
+          </div>
+          <div class="column-nav w-col w-col-10">
+            Bonds
+          </div>
+        </div>
+      </router-link>
+      <router-link :to="{name: 'about'}" class="tungsten-nav-link">
+        <div class="w-row">
+          <div class="w-col w-col-2">
+            <fa-icon icon="question-circle" size="2x" class="nav-icon"/>
+          </div>
+          <div class="column-nav w-col w-col-10">
+            About
+          </div>
+        </div>
+      </router-link>
+      <div class="social-footer">
+        <div class="w-row">
+          <div class="w-col w-col-12">
+            <div v-if="!account">
+              <a href="#" @click="pairScatter()" class="pair-link">
+                Pair with your
+                <img src="@/assets/images/scatter.png" width="65" alt="Scatter" class="pair"/>
+              </a>
+            </div>
+            <div v-else class="account-info">
+              <p>
+                Account: <strong>{{account.name}}</strong>
+              </p>
+              <div v-if="!loadingActiveAuthority" style="margin-top: 12px">
+                <a v-if="!hasGrantedPermission" href="#" @click="grantPermission()" class="sidebar-button white">Grant Permission</a>
+                <a v-else href="#" @click="removePermission()" class="sidebar-button white">Remove Permission</a>
+              </div>
+              <div style="margin-top: 12px">
+                <a href="#" @click="logOut()" class="sidebar-button white">Log out</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="columns-8 w-row">
+          <div class="w-col w-col-4">
+            <a href="#" class="sidebar-button white">Help</a>
+          </div>
+          <div class="column-5 w-col w-col-4">
+            <h2 class="sidebar-links">Privacy</h2>
+          </div>
+          <div class="column-6 w-col w-col-4">
+            <h2 class="bond-expire">Terms</h2>
+          </div>
+        </div> -->
+      </div>
+    </nav>
+</template>
+
+<script>
+import { mapActions, mapState, mapGetters } from "vuex";
+
+export default {
+  name: "Nav",
+  computed: {
+    ...mapState(["account", "loadingActiveAuthority"]),
+    ...mapGetters(["hasGrantedPermission"])
+  },
+  methods: {
+    ...mapActions([
+      "pairScatter",
+      "logOut",
+      "grantPermission",
+      "removePermission"
+    ])
+  }
+};
+</script>
+
+<style>
+.bm-menu {
+  background-color: #a8b5c0;
+}
+</style>
+
+<style scoped>
+.bm-menu {
+  background-color: #d8e4ed;
+}
+.tungsten-nav-menu {
+  display: block;
+  padding-left: 35px;
+}
+@media (max-width: 991px) {
+  .tungsten-nav-menu {
+    padding-left: 0;
+  }
+}
+.tungsten-nav-link {
+  display: block;
+  margin-left: 0px;
+  padding: 5px 0px;
+  font-family: "Varela Round", sans-serif;
+  color: #0f2e4d;
+  font-size: 15px;
+  font-weight: 800;
+  text-align: left;
+  letter-spacing: 0.35px;
+  text-transform: none;
+}
+.router-link-exact-active {
+  color: #007fd3;
+}
+.nav-icon {
+  margin-top: 6px;
+  margin-bottom: 6px;
+}
+.column-nav {
+  margin-top: 12px;
+}
+.social-footer {
+  margin-top: 48px;
+}
+.pair-link {
+  text-decoration: none;
+  color: #0f2e4d;
+}
+.pair {
+  display: block;
+  width: 131px;
+  margin-top: 12px;
+}
+.account-info {
+  color: #0f2e4d;
+}
+</style>
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,118 +1,28 @@
 <template>
   <div data-collapse="medium" data-animation="default" data-duration="400" data-no-scroll="1" class="left-navigation w-nav">
-    <a href="#" class="logo-link w-nav-brand">
+    <router-link :to="{name: 'home'}" class="logo-link w-nav-brand">
       <h1 class="brand-text logo">Tungsten</h1>
       <div class="credits">
         By <img src="@/assets/images/eosmetal-logo.png" width="80px" alt="EOSMetal"/>
       </div>
-    </a>
-    <nav role="navigation" class="nav-menu w-nav-menu">
-      <router-link :to="{name: 'home'}" class="nav-link w-nav-link">
-        <div class="columns-5 w-row">
-          <div class="column-11 w-col w-col-2">
-            <fa-icon icon="home" size="2x" class="nav-icon"/>
-          </div>
-          <div class="column-nav w-col w-col-10">
-            Home
-          </div>
-        </div>
-      </router-link>
-      <router-link :to="{name: 'bonds'}" class="nav-link w-nav-link">
-        <div class="columns-5 w-row">
-          <div class="column-11 w-col w-col-2">
-            <fa-icon icon="link" size="2x" class="nav-icon"/>
-          </div>
-          <div class="column-nav w-col w-col-10">
-            Bonds
-          </div>
-        </div>
-      </router-link>
-      <router-link :to="{name: 'about'}" class="nav-link w-nav-link">
-        <div class="columns-5 w-row">
-          <div class="column-11 w-col w-col-2">
-            <fa-icon icon="question-circle" size="2x" class="nav-icon"/>
-          </div>
-          <div class="column-nav w-col w-col-10">
-            About
-          </div>
-        </div>
-      </router-link>
-    </nav>
-    <div class="hamburger-button w-nav-button">
-      <div class="icon w-icon-nav-menu"></div>
-    </div>
-    <div class="social-footer w-hidden-medium w-hidden-small w-hidden-tiny">
-      <div v-if="!account">
-        <a href="#" @click="pairScatter()" class="footer-pair w-nav-link">
-          Pair with your
-          <img src="@/assets/images/scatter.png" width="65" alt="Scatter" class="pair"/>
-        </a>
-      </div>
-      <div v-else>
-        <p>
-          Account: <strong>{{account.name}}</strong>
-        </p>
-        <div v-if="!loadingActiveAuthority" style="margin-top: 12px">
-          <a v-if="!hasGrantedPermission" href="#" @click="grantPermission()" class="sidebar-button white">Grant Permission</a>
-          <a v-else href="#" @click="removePermission()" class="sidebar-button white">Remove Permission</a>
-        </div>
-        <div style="margin-top: 12px">
-          <a href="#" @click="logOut()" class="sidebar-button white">Log out</a>
-        </div>
-      </div>
-      <!-- <div class="columns-8 w-row">
-        <div class="w-col w-col-4">
-          <a href="#" class="sidebar-button white">Help</a>
-        </div>
-        <div class="column-5 w-col w-col-4">
-          <h2 class="sidebar-links">Privacy</h2>
-        </div>
-        <div class="column-6 w-col w-col-4">
-          <h2 class="bond-expire">Terms</h2>
-        </div>
-      </div> -->
-    </div>
+    </router-link>
+    <Nav class="w-hidden-medium w-hidden-small w-hidden-tiny"/>
+    <Slide class="hamburger">
+      <Nav/>
+    </Slide>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState, mapGetters } from "vuex";
+import Nav from "./Nav";
+import { Slide } from "vue-burger-menu";
 
 export default {
-  name: "Sidebar",
-  computed: {
-    ...mapState(["account", "loadingActiveAuthority"]),
-    ...mapGetters(["hasGrantedPermission"])
-  },
-  methods: {
-    ...mapActions([
-      "pairScatter",
-      "logOut",
-      "grantPermission",
-      "removePermission"
-    ])
-  }
+  components: { Nav, Slide }
 };
 </script>
 
 <style scoped>
-.router-link-exact-active {
-  margin-left: 0px;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  font-family: "Varela Round", sans-serif;
-  color: #007fd3;
-  font-weight: 800;
-  text-align: left;
-  letter-spacing: 0.35px;
-  text-transform: capitalize;
-}
-@media (max-width: 479px) {
-  .router-link-exact-active {
-    margin-left: 28px;
-    color: #0f2e4d;
-  }
-}
 .credits {
   margin-top: -18px;
   margin-bottom: 42px;
@@ -121,15 +31,24 @@ export default {
 .credits > img {
   margin-left: 6px;
 }
-.nav-icon {
-  margin-top: 6px;
-  margin-bottom: 6px;
+@media (min-width: 992px) {
+  .hamburger {
+    display: none;
+  }
 }
-.column-nav {
-  margin-top: 12px;
-}
-.pair {
-  margin-left: 0;
-  margin-top: 12px;
+@media (max-width: 991px) {
+  .logo-link {
+    margin-left: 82px;
+  }
+  .brand-text {
+    display: inline-block;
+  }
+  .credits {
+    display: inline-block;
+    vertical-align: middle;
+    margin: 0;
+    margin-left: 16px;
+    margin-top: -8px;
+  }
 }
 </style>
